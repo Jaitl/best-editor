9cc7650af67c7fae188684f2f0b3dfe66b209ee0
Jaitl <Jaitl@outlook.com>
2015-02-28 20:03:55 +0300

    properties save to file

--- src/main/java/com/jaitlapps/besteditor/CommonPreferences.java
+++ src/main/java/com/jaitlapps/besteditor/CommonPreferences.java
@@ -1,9 +1,16 @@
 package com.jaitlapps.besteditor;
 
-import java.util.prefs.Preferences;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Properties;
 
 public class CommonPreferences {
-    private Preferences preferences;
+
+    private static final FileProperties preferences = new FileProperties();
 
     public static final String WORK_FOLDER = "work_folder";
     public static final String RESENT_ICON_FOLDER = "resent_icon_folder";
@@ -17,9 +24,7 @@ public class CommonPreferences {
         return INSTANCE;
     }
 
-    private CommonPreferences() {
-        preferences = Preferences.userNodeForPackage(Preferences.class);
-    }
+    private CommonPreferences() { }
 
     public void putWorkFolder(String url) {
         preferences.put(WORK_FOLDER, url);
@@ -59,5 +64,81 @@ public class CommonPreferences {
 
     public String getMarkdownEditor() {
         return preferences.get(MARKDOWN_EDITOR_EXE, null);
+    }
+
+    private static class FileProperties {
+
+        private static org.slf4j.Logger log = LoggerFactory.getLogger(FileProperties.class);
+
+        private final Properties properties = new Properties();
+
+        public FileProperties() {
+            loadPropertiesFromFile();
+        }
+
+        public String get(String key, String defaultValue) {
+            return properties.getProperty(key, defaultValue);
+        }
+
+        public void put(String key, String value) {
+            properties.setProperty(key, value);
+            storePropertiesToFile();
+        }
+
+        private void loadPropertiesFromFile() {
+            try {
+                InputStream is = new FileInputStream(getPathToFile());
+
+                properties.load(is);
+            } catch (FileNotFoundException e) {
+                log.error("error", e);
+            } catch (IOException e) {
+                log.error("error", e);
+            }
+        }
+
+        private void storePropertiesToFile() {
+            try {
+                OutputStream os = new FileOutputStream(getPathToFile());
+                properties.store(os, "");
+            } catch (FileNotFoundException e) {
+                log.error("error", e);
+            } catch (IOException e) {
+                log.error("error", e);
+            }
+        }
+
+        private String getPathToFile() {
+            File jar = null;
+
+            try {
+                 jar = new File (FileProperties.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
+            } catch (URISyntaxException e) {
+                log.error("error", e);
+            }
+
+            Path mainPath = jar.getParentFile().toPath();
+            Path pathToSettings = mainPath.resolve("settings");
+
+            if(Files.notExists(pathToSettings)) {
+                try {
+                    Files.createDirectory(pathToSettings);
+                } catch (IOException e) {
+                    log.error("error", e);
+                }
+            }
+
+            pathToSettings = pathToSettings.resolve("best.properties");
+
+            if(Files.notExists(pathToSettings)) {
+                try {
+                    Files.createFile(pathToSettings);
+                } catch (IOException e) {
+                    log.error("error", e);
+                }
+            }
+
+            return pathToSettings.toString();
+        }
     }
 }
